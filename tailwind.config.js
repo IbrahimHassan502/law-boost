3c7e0f7ebff8936d2bfc43d0625246ffd07d208d
IbrahimHassan502 <ih35840@gmail.com>
2023-10-10 07:43:05 +0200

    started hero and navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./public/js/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "main-font": ["Manrope", "sans-serif"],
+        "heading-font": ["Gilroy", "sans-serif"],
+      },
+      colors: {
+        "yellow-color": "#fcec52",
+      },
+    },
   },
   plugins: [],
 };

27f3d033912f86250803132351fe9cb0cdf1dd37
IbrahimHassan502 <ih35840@gmail.com>
2023-10-10 15:55:38 +0200

    done responsive nav menu and done lg screen hero

--- public/js/main-script.js
+++ public/js/main-script.js
@@ -1,1 +1,9 @@
 "use strict";
+// mobile menu functionality
+const mobileMenuButton = document.querySelector(".mobile-menu-button");
+const navMenu = document.querySelector(".nav-menu");
+
+mobileMenuButton.addEventListener("click", () => {
+  navMenu.classList.toggle("-translate-y-full");
+  navMenu.classList.toggle("opacity-0");
+});

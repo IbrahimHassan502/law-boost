523915ace193e2a237efff947a6dffe19f2ce95d
IbrahimHassan502 <ih35840@gmail.com>
2023-10-12 00:50:04 +0200

    finished why you'll love us sectino, ai, and graph section with responsive

--- public/js/main-script.js
+++ public/js/main-script.js
@@ -7,3 +7,7 @@ mobileMenuButton.addEventListener("click", () => {
   navMenu.classList.toggle("-translate-y-full");
   navMenu.classList.toggle("opacity-0");
 });
+// instution reivews owl carosuel
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
